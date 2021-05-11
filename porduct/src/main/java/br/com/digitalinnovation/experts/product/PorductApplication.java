7b29b438959caa6a0bad8a383c6245a948d26485
anonymous <jcolivati@gmail.com>
2021-05-11 18:27:18 -0300

    Created Product Project

---
+++ porduct/src/main/java/br/com/digitalinnovation/experts/product/PorductApplication.java
@@ -0,0 +1,13 @@
+package br.com.digitalinnovation.experts.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorductApplication.class, args);
+	}
+
+}
